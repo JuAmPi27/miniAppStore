6cae662a0b581490b5c5c42b00f4d26e09754bb3
Juampi27 <Jpbarnetche27@gmail.com>
2022-02-02 17:09:48 -0300

    componente header y products, servicio products, serverAPI e interface Product

---
+++ src/app/pages/products/products.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { tap } from 'rxjs/operators';
+import { ProductsService } from './services/products.service';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+
+  constructor(private productSvc: ProductsService) { }
+
+  ngOnInit(): void {
+    this.productSvc.getProducts()
+    .pipe(tap(res =>console.log(res)))
+    .subscribe()
+  }
+
+}
