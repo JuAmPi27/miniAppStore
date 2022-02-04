3bf8af0b03952b98d2491c33816b2a5fdfd74fea
Juampi27 <Jpbarnetche27@gmail.com>
2022-02-04 19:59:02 -0300

    interface order

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { tap } from 'rxjs/operators';
+import { NgForm } from '@angular/forms';
+import { switchMap, tap } from 'rxjs/operators';
+import { Details, Order } from 'src/app/shared/interfaces/order.interface';
 import { Store } from 'src/app/shared/interfaces/stores.interface';
 import { DataService } from 'src/app/shared/services/data.service';
+import { ShoppingCartService } from 'src/app/shared/services/shopping-cart-service';
+import { Product } from '../products/interfaces/product.interface';
 
 @Component({
   selector: 'app-checkout',
@@ -18,25 +22,67 @@ export class CheckoutComponent implements OnInit {
     city: "",
   };
 
+  cart: Product[] = [];
   isDelivery = false;
   stores: Store[] = [];
 
-  constructor(private dataSvc: DataService) { }
+  constructor(private dataSvc: DataService, private shoppingCartSvc: ShoppingCartService) { }
 
   ngOnInit(): void {
     this.getStores();
+    this.getDataCart();
+    this.prepareDetails();
   }
 
   onPickUpOrDelivery(value: boolean): void {
     this.isDelivery = value;
   }
 
-  onSubmit(): void {
-    console.log("guardar");
+  onSubmit({ value: formData }: NgForm ): void {
+    console.log("guardar", formData);
+    const data: Order = {
+      ...formData,
+      date: this.getCurrentDay(),
+      pickUp: this.isDelivery,
+    }
+    this.dataSvc.saveOrders(data)
+    .pipe( 
+      tap(res => console.log("order=>", res)),
+      switchMap((order) => {
+        const orderId = order.id;
+        const details = this.prepareDetails();
+        return this.dataSvc.saveDetailsOrder({ details, orderId });
+  }),
+  tap(res => console.log("Finish=>", res)),
+    )
+  .subscribe();
   }
 
   private getStores(): void { // ver que hace el operador tap y como desubscribrse de un observable
     this.dataSvc.getStore().pipe( tap((stores: Store[]) => this.stores = stores)).subscribe();
   }
 
+  private getCurrentDay(): string {
+    return new Date().toLocaleDateString();
+  }
+
+  private prepareDetails(): Details[] {
+    const details: Details[] = [];
+    this.cart.forEach( (product: Product) => {
+      const {id: productId, name: productName, quantity, stock} = product;
+      details.push({ productId, productName, quantity });
+    });
+    return details;
+  }
+
+  private getDataCart(): void {
+    this.shoppingCartSvc.cartAction$
+    .pipe(
+      tap((products: Product[]) => this.cart = products)
+    )
+    .subscribe();
+  }
+
 }
+
+
--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { DetailsOrder, Order } from '../interfaces/order.interface';
 import { Store } from '../interfaces/stores.interface';
 
 @Injectable({
@@ -12,7 +13,17 @@ export class DataService {
   constructor(private http: HttpClient) { }
 
   getStore(): Observable<Store[]> {
-    return this.http.get<Store[]>(`${this.apiURL}/stores`);
+    return this.http.get<Store[]>(`${this.apiURL}/stores`); //bakstits con alt 96
+  }
+
+  // lo ideal seria que una clase especifica se encargara de las orders pero en este caso al ser una demo la hacemos dentro de esta
+  saveOrders(order: Order): Observable<Order> {
+    return this.http.post<any>(`${this.apiURL}/orders`, order)
+  }
+
+  // si tuvieramos un back end este metodo no seria necesario
+  saveDetailsOrder (details: DetailsOrder): Observable<DetailsOrder> {
+    return this.http.post<DetailsOrder>(`${this.apiURL}/detailsOrders`, details)
   }
 
 }
