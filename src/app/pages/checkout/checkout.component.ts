65a9581f84709d41d40ee1ee3b6c78bc1f72e24c
Juampi27 <Jpbarnetche27@gmail.com>
2022-02-04 17:01:23 -0300

    data service, modificaciones y funcionalidades en el formulario en componente checkout

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -1,47 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { tap } from 'rxjs/operators';
+import { Store } from 'src/app/shared/interfaces/stores.interface';
+import { DataService } from 'src/app/shared/services/data.service';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
+
 export class CheckoutComponent implements OnInit {
 
   model = {
     name: "",
     store: "",
     shippingAddress: "",
-    city: ""
+    city: "",
   };
 
-  stores = [
-    {
-      "id": 1,
-      "name": "Park Row at Beekman St",
-      "address": "38 Park Row",
-      "city": "New York",
-      "openingHours": "10:00 - 14:00 and 17:00 - 20:30"
-    },
-    {
-      "id": 2,
-      "name": "Store Alcalá",
-      "address": "Calle de Alcalá, 21",
-      "city": "Madrid",
-      "openingHours": "10:00 - 14:00 and 17:00 - 20:30"
-    }
-  ]
+  isDelivery = false;
+  stores: Store[] = [];
 
-  constructor() { }
+  constructor(private dataSvc: DataService) { }
 
   ngOnInit(): void {
+    this.getStores();
   }
 
   onPickUpOrDelivery(value: boolean): void {
-    
+    this.isDelivery = value;
   }
 
   onSubmit(): void {
+    console.log("guardar");
+  }
 
+  private getStores(): void { // ver que hace el operador tap y como desubscribrse de un observable
+    this.dataSvc.getStore().pipe( tap((stores: Store[]) => this.stores = stores)).subscribe();
   }
 
 }
